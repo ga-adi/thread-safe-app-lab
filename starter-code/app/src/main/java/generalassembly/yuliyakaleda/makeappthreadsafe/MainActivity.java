871fc7e53bd9c5bb3146afaf9c5532876093326a
David Sarker <dsarker27@gmail.com>
2016-02-24 07:31:35 -0500

    Added AsyncTask

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/makeappthreadsafe/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/makeappthreadsafe/MainActivity.java
@@ -4,81 +4,104 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity{
-  private static final String TAG = "makeappthreadsafe";
-  private static final int PICK_IMAGE_REQUEST = 1;
-  private ImageView image;
-  private Button change;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "makeappthreadsafe";
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView image;
+    private Button change;
+    private Uri mSelectedImage;
+    private ProgressBar mProgressBar;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    change = (Button) findViewById(R.id.choose_button);
-    image = (ImageView) findViewById(R.id.image);
-    setProfileImage();
-    change.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        changeProfileImage();
-      }
-    });
-  }
-
-  // sets the chosen image as a profile picture
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
-      Uri selectedImage = data.getData();
-      image.setImageURI(selectedImage);
-
-      //saves a new picture to a file
-      Bitmap bitmap = null;
-      try {
-        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage));
-      } catch (FileNotFoundException e) {
-        Log.d(TAG, "Image uri is not received or recognized");
-      }
-      try {
-        PictureUtil.saveToCacheFile(bitmap);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
-      //provides a feedback that the image is set as a profile picture
-      Toast.makeText(this, "The image is set as a profile picture", Toast.LENGTH_LONG).show();
+        change = (Button) findViewById(R.id.choose_button);
+        image = (ImageView) findViewById(R.id.image);
+        setProfileImage();
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeProfileImage();
+            }
+        });
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
-  }
 
-  //sets the image view of the profile picture to the previously saved image or the placeholder if
-  // the image has never been modified
-  private void setProfileImage() {
-    Bitmap bm = PictureUtil.loadFromCacheFile();
-    if (bm != null) {
-      image.setImageBitmap(bm);
-    } else {
-      image.setImageResource(R.drawable.placeholder);
+    // sets the chosen image as a profile picture
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
+            mSelectedImage = data.getData();
+            image.setImageURI(mSelectedImage);
+
+            MyTask myTask = new MyTask();
+            myTask.execute();
+        }
     }
-  }
 
-  // brings up the photo gallery/other resources to choose a picture
-  private void changeProfileImage() {
-    Intent intent = new Intent();
-    intent.setType("image/*");
-    intent.setAction(Intent.ACTION_GET_CONTENT);
-    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-  }
+    //sets the image view of the profile picture to the previously saved image or the placeholder if
+    // the image has never been modified
+    private void setProfileImage() {
+        Bitmap bm = PictureUtil.loadFromCacheFile();
+        if (bm != null) {
+            image.setImageBitmap(bm);
+        } else {
+            image.setImageResource(R.drawable.placeholder);
+        }
+    }
+
+    // brings up the photo gallery/other resources to choose a picture
+    private void changeProfileImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(mSelectedImage));
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "Image uri is not received or recognized");
+            }
+            try {
+                PictureUtil.saveToCacheFile(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mProgressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, "The image is set as a profile picture", Toast.LENGTH_LONG).show();
+        }
+    }
 }
